f4151872346d8f1706f56c9a20c33b74488c5d37
Michael Purwin <mpurwin@gmail.com>
2019-03-26 12:57:08 -0400

    Set site title, add plugins
    Add Google fonts plugin, sass plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mike Purwin`,
+    description: `Ya making things!`,
+    author: `Mike Purwin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,23 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Work Sans`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require("sass"),
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

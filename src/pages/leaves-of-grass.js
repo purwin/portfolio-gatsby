30aab240773eba7f69ae259d2fcc417b9396f682
Michael Purwin <mpurwin@gmail.com>
2019-03-27 09:38:59 -0400

    Init Leaves of Grass Page

---
+++ src/pages/leaves-of-grass.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Work from "../components/templates/work"
+
+
+const LeavesPage = () => {
+	const query = graphql`
+    query {
+      imgs:allFile(filter:{relativePath:{regex:"/^ebook-leaves-\\\\d.+png/"}}, sort:{fields:name}) {
+        edges {
+          node {
+          childImageSharp {
+            fluid(maxWidth:1200) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+          }
+        }
+      }
+    }
+  `;
+
+	return (
+		<StaticQuery
+			query={query}
+			render={data => {
+
+        const imgs = data.imgs.edges;
+        const leaves_cover = imgs.shift();
+
+        console.log(imgs);
+        
+        return (
+          <Work
+            title={`Leaves of Grass`}
+            cover={<Img fluid={leaves_cover.node.childImageSharp.fluid} />}
+            bio={
+              [
+                `“The Deathbed Edition”. An ebook of Mr. Whitman's epic 293-poem collection, broken up and styled from a .txt file using only RegEx.`,
+              ]
+            }
+            body={
+              <div className="feature__body">
+                <div className="body__row">
+                  {imgs.map(({node}) => (
+                    <div
+                      key={node.id}
+                      className={`body__item body__item--full`}
+                    >
+                      <Img fluid={node.childImageSharp.fluid} />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            }
+          />
+			)}}
+		/>
+	)
+};
+
+export default LeavesPage;

744ae7f4c05ef914001d26deae128fc6c96accda
Michael Purwin <mpurwin@gmail.com>
2019-03-27 12:58:12 -0400

    Init Work Page

---
+++ src/pages/work.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/templates/layout"
+import SEO from "../components/seo"
+
+const WorkPage = () => (
+  <Layout>
+    <SEO title="Work" keywords={[`gatsby`, `application`, `react`]} />
+  </Layout>
+)
+
+export default WorkPage

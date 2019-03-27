091ed1e0fc23ec6e671fcd7e9cf39830dca6f085
Michael Purwin <mpurwin@gmail.com>
2019-03-27 19:28:51 -0400

    Update Header to a class component

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,38 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { Component } from "react"
 
 import './header.scss'
 
-const Header = ({ siteTitle }) => {
-  const coolThings = ['ebooks', 'websites', 'graphics', 'GIFs', 'buttons', 'web apps', 'risographs', 'videos', 'applescripts', 'printed matter', 'posters', 'python scripts', 'databases', 'tables', 'desks', 'shelves', 'beer'];
+class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      coolThing: this.coolThings[0]
+    }
+  }
 
-  // const showCoolThings = () => {
+  coolThings = [`ebooks`, `websites`, `graphics`, `GIFs`, `buttons`, `web apps`, `risographs`, `videos`, `applescripts`, `printed matter`, `posters`, `python scripts`, `databases`, `tables`, `desks`, `shelves`, `beer`]
 
-  // };
-  return (
-    <header>
-      <div>
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to="/"
-          >
-            {siteTitle}
-          </Link>
-        </h1>
-        <p className="header__p">
-          <span className="bio">makes </span>
-          <span className="bio"><span id="id_noun">{coolThings[0]}</span>.</span>
-        </p>
-      </div>
-    </header>
-  )
+  render() {
+    return (
+      <header>
+        <div>
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+            >
+              {this.props.siteTitle}
+            </Link>
+          </h1>
+          <p className="header__p">
+            <span className="bio">makes </span>
+            <span className="bio"><span id="id_noun">{this.state.coolThing}</span>.</span>
+          </p>
+        </div>
+      </header>
+    )
+  }
 }
 
 Header.propTypes = {

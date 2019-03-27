ff5fc7fbf4823cc82e4eae9085fe2b7d82f36685
Michael Purwin <mpurwin@gmail.com>
2019-03-27 11:00:58 -0400

    Init Ebooks page

---
+++ src/pages/ebooks.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/templates/layout'
+import SEO from '../components/seo'
+
+const EbooksPage = () => (
+  <Layout display={`ebooks`}>
+    <SEO title="Ebooks" keywords={[`gatsby`, `application`, `react`]} />
+  </Layout>
+)
+
+export default EbooksPage

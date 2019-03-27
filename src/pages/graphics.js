d32a6a7249b8f2510d57d6c08d48798da42beaf0
Michael Purwin <mpurwin@gmail.com>
2019-03-27 12:57:59 -0400

    Init Graphics Page

---
+++ src/pages/graphics.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/templates/layout'
+import SEO from '../components/seo'
+
+const GraphicsPage = () => (
+  <Layout display={`graphics`}>
+    <SEO title="Graphics" keywords={[`gatsby`, `application`, `react`]} />
+  </Layout>
+)
+
+export default GraphicsPage

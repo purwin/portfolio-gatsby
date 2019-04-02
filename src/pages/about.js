2cf85eb86f5ca743e7d250dedd0769c7bb92ce26
Michael Purwin <mpurwin@gmail.com>
2019-04-01 23:15:54 -0400

    Replaced Work template with Layout template

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-import Work from "../components/templates/work"
+import Layout from '../components/templates/layout'
+import SEO from '../components/seo'
 
 const AboutPage = () => {
 
@@ -18,52 +19,57 @@ const AboutPage = () => {
     }
   `;
 
-  const Body = () => (
-    <>
-      <div className={`body__row`}>
-        <div className={`body__item body__item--r`}>
-          <h3>Hello! I am [a/n]:</h3>
-          <ul>
-            <li>Ebook Designer at Penguin Random House</li>
-            <li>Native son of New Jersey</li>
-            <li>Jackson Heightsian working in New York City</li>
-            <li>Experienced in HTML, CSS, JavaScript, Python, SQL, among others</li>
-            <li>Frequent camper</li>
-            <li>Emergency carpenter</li>
-            <li>Future welder</li>
-          </ul>
+  const Body = ({cover}) => (
+    <div className={`feature about`}>
+      <div className={`feature__cover`}>
+        {cover}
+      </div>
+      <div className={`feature__body`}>
+        <div className={`body__row`}>
+          <div className={`body__item body__item--r`}>
+            <h3>Hello! I am [a/n]:</h3>
+            <ul>
+              <li>Ebook Designer at Penguin Random House</li>
+              <li>Native son of New Jersey</li>
+              <li>Jackson Heightsian working in New York City</li>
+              <li>Experienced in HTML, CSS, JavaScript, Python, SQL, among others</li>
+              <li>Frequent camper</li>
+              <li>Emergency carpenter</li>
+              <li>Future welder</li>
+            </ul>
+          </div>
+        </div>
+        <div className={`body__row`}>
+          <div className={`body__item`}>
+            <h3>I like to:</h3>
+            <ul>
+              <li>Make websites</li>
+              <li>Build databases for fun</li>
+              <li>Design sensible interfaces</li>
+            </ul>
+          </div>
+        </div>
+        <div className={`body__row`}>
+          <div className={`body__item body__item--r`}>
+            <h3>Contact!</h3>
+            <ul>
+              <li>mpurwin@gmail.com</li>
+              <li><a href="https://github.com/purwin">GitHub</a></li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className={`body__row`}>
-        <div className={`body__item`}>
-          <h3>I like to:</h3>
-          <ul>
-            <li>Make websites</li>
-            <li>Build databases for fun</li>
-            <li>Design sensible interfaces</li>
-          </ul>
-        </div>
-      </div>
-      <div className={`body__row`}>
-        <div className={`body__item body__item--r`}>
-          <h3>Contact!</h3>
-          <ul>
-            <li>mpurwin@gmail.com</li>
-            <li><a href="https://github.com/purwin">GitHub</a></li>
-          </ul>
-        </div>
-      </div>
-    </>
+    </div>
   );
 
   return (
     <StaticQuery
       query={query}
       render={data => (
-        <Work
-          cover={<Img fluid={data.file.childImageSharp.fluid} />}
-          body={<Body />}
-        />
+        <Layout display={`none`}>
+          <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
+          <Body cover={<Img fluid={data.file.childImageSharp.fluid} />} />
+        </Layout>
       )}
     />
   );

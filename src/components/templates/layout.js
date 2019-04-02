792d6d9453c646c0dec64ad67dcc397c45029a28
Michael Purwin <mpurwin@gmail.com>
2019-04-01 23:12:44 -0400

    Remove 'required' propType for children prop

--- src/components/templates/layout.js
+++ src/components/templates/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
@@ -41,7 +34,6 @@ const Layout = ({ children, display }) => (
 )
 
 Layout.propTypes = {
-  // children: PropTypes.node.isRequired,
   children: PropTypes.node,
 }
 

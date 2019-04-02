940cba4af4a360ddd16f7e59d5bbd26d550099f0
Michael Purwin <mpurwin@gmail.com>
2019-04-01 23:15:14 -0400

    Init Ellipsis Page

---
+++ src/pages/ellipsis.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Work from "../components/templates/work"
+
+const NYCParksPage = () => {
+  const title = `Ellipsis Press`
+
+  const bio = [`Promo material for a 👍👍👍 independent publisher.`]
+
+  const query = graphql`
+    query {
+      imgs:allFile(filter:{relativePath:{regex:"/^ellipsis-\\\\d+.+png$/"}},
+      sort:{fields:name}) {
+        edges {
+          node {
+            id
+           childImageSharp {
+             fluid(maxWidth:1200) {
+              ...GatsbyImageSharpFluid
+             }
+           }
+          }
+        }
+      }
+    }
+  `;
+
+  return (
+    <StaticQuery
+      query={query}
+      render={data => {
+        const imgs = data.imgs.edges;
+
+        const Body = () => (
+          imgs.map(({node}, index) => (
+            <div
+              key={index}
+              className={`body__row`}
+            >
+              <div
+                key={node.id}
+                className={`body__item body__item--c`}
+              >
+                <Img fluid={node.childImageSharp.fluid} />
+              </div>
+            </div>
+          ))
+        )
+
+        return (
+          <Work
+            title={title}
+            bio={bio}
+            body={<Body />}
+          />
+        );}
+      }
+    />
+  );
+
+}
+
+export default NYCParksPage;

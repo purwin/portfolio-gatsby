8d2b4dc7b2f1b7f063c1630fa4c67714bf4a3f91
Michael Purwin <mpurwin@gmail.com>
2019-04-05 18:28:38 -0400

    Add gatsby-plugin-transition-link

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,8 +43,6 @@ module.exports = {
         implementation: require("sass"),
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    `gatsby-plugin-transition-link`
   ],
 }
